f45b318385bbc9bbb4a66dfaccb0ce8f5fd4268f
execut4ble <jokubas.banaitis@gmail.com>
2021-02-10 21:55:41 +0200

    Fix comment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15, // limit each IP to 10 requests per windowMs
+  max: 15, // limit each IP to 15 requests per windowMs
 });
 
 app.use(express.json());
